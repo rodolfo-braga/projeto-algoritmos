b9002e7a3a0955c52f5da8381ffdab3c7e01e1dd
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-07-11 10:21:23 -0300

    feat: implements function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        counter = 0
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
